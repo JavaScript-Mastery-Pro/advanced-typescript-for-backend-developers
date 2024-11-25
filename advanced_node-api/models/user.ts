6a7ec4273a17db6ce16293762577641b09a8fc0a
sujatagunale <sujata@jsmastery.pro>
2024-11-25 23:01:36 +0530

    advanced_node-api-user-model

--- advanced_node-api/models/user.ts
+++ advanced_node-api/models/user.ts
@@ -1,1 +1,32 @@
-// implement User model
+import { Schema, Document, model } from "mongoose";
+
+export enum Role {
+  Admin = "admin",
+  User = "user",
+}
+
+export interface IUser {
+  name: string;
+  email: string;
+  role: Role;
+}
+
+export interface IUserDocument extends IUser, Document {}
+
+const UserSchema: Schema = new Schema<IUser>(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    role: {
+      type: String,
+      enum: Object.values(Role),
+      required: true,
+      default: Role.User,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = model<IUser>("User", UserSchema);
+
+export default User;
