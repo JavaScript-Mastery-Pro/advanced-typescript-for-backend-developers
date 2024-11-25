4577ca28e97408d202b869197679196eadb88f56
sujatagunale <sujata@jsmastery.pro>
2024-11-25 23:09:10 +0530

    advanced_node-api_05-user-service

--- advanced_node-api/services/user.ts
+++ advanced_node-api/services/user.ts
@@ -1,1 +1,24 @@
-// implement user service
+import { injectable } from "inversify";
+import User, { IUser, IUserDocument } from "@/models/user";
+
+@injectable()
+export class UserService {
+  public async getUsers(): Promise<IUserDocument[]> {
+    return User.find();
+  }
+
+  public async createUser(userData: Partial<IUser>): Promise<IUserDocument> {
+    return User.create(userData);
+  }
+
+  public async updateUser(
+    id: string,
+    updateData: Partial<IUser>
+  ): Promise<IUserDocument | null> {
+    return User.findByIdAndUpdate(id, updateData, { new: true });
+  }
+
+  public async deleteUser(id: string): Promise<IUserDocument | null> {
+    return User.findByIdAndDelete(id);
+  }
+}
