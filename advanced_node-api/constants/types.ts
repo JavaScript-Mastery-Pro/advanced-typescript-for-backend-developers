f7fb8f4eb23c76028492cce1db3d21672ede8bc7
sujatagunale <sujata@jsmastery.pro>
2024-11-25 23:13:19 +0530

    advanced_node-api_06-symbol-types

--- advanced_node-api/constants/types.ts
+++ advanced_node-api/constants/types.ts
@@ -1,1 +1,9 @@
-// define types
+const TYPES = {
+  UserService: Symbol.for("UserService"),
+  LogService: Symbol.for("LogService"),
+  LoggerMiddleware: Symbol.for("LoggerMiddleware"),
+  GenericService: Symbol.for("GenericService"),
+  EventService: Symbol.for("EventService"),
+};
+
+export default TYPES;
